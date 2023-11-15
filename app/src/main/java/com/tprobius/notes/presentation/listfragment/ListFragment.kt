c1498299d87358868d4909833381a17e1b4fa51e
Заремба Виктория <dviktoriyas@gmail.com>
2023-11-15 20:45:01 +0700

    Added list fragment layout

--- app/src/main/java/com/tprobius/notes/presentation/listfragment/ListFragment.kt
+++ app/src/main/java/com/tprobius/notes/presentation/listfragment/ListFragment.kt
@@ -1,6 +1,23 @@
 package com.tprobius.notes.presentation.listfragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.tprobius.notes.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+    private var _binding: FragmentListBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) { "Binding isn't initialized" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
