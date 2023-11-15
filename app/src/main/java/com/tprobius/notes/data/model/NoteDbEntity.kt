234a885a7c82e6f730d95a99b1276b7ef23ee1cf
Заремба Виктория <dviktoriyas@gmail.com>
2023-11-15 20:36:08 +0700

    Added note entity for database

---
+++ app/src/main/java/com/tprobius/notes/data/model/NoteDbEntity.kt
@@ -0,0 +1,19 @@
+package com.tprobius.notes.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.tprobius.notes.domain.entities.Note
+
+@Entity(tableName = "notes")
+data class NoteDbEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val title: String,
+    val content: String
+) {
+    fun mapToNote() = Note(id, title, content)
+
+    companion object {
+        fun mapToNoteDbEntity(note: Note) = NoteDbEntity(note.id, note.title, note.content)
+    }
+}
